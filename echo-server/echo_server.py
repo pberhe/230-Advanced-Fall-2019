36cadbbf24150f44c045dfb882e07b2cd76e22f8
Joseph Schilz <joseph@schilz.org>
2019-10-05 19:12:59 -0700

    Update echo_server.py
    The `bind` tells the server what address and port it should listen to, but then the `listen` tells the server to actually begin listening. :)

--- echo-server/echo_server.py
+++ echo-server/echo_server.py
@@ -23,6 +23,7 @@ def server(log_buffer=sys.stderr):
     # TODO: bind your new sock 'sock' to the address above and begin to listen
     #       for incoming connections
     sock.bind(address)
+    sock.listen(1)
 
     try:
         # the outer loop controls the creation of new connection sockets. The
